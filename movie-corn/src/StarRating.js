e6f29b080437a6ad2abb7f763cd31e546098c54b
SrestMilan <shrestham780@gmail.com>
2024-07-18 07:50:30 +0100

    destructure concept to be implement for reusability of componenet

--- movie-corn/src/StarRating.js
+++ movie-corn/src/StarRating.js
@@ -1,3 +1,18 @@
 export default function StarRating() {
-  return <div> Hello</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "16px",
+      }}
+    >
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+        <p>10</p>
+      </div>
+    </div>
+  );
 }
