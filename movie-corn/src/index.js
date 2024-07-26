fb4581af7de61b5b7a3ba08d6a06968e91df3ff9
SrestMilan <shrestham780@gmail.com>
2024-07-26 11:08:45 +0100

    fecthing data of movie

--- movie-corn/src/index.js
+++ movie-corn/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating size={24} color="red" class="test" /> */}
   </React.StrictMode>
 );
